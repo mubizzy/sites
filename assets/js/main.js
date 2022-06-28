a9b46b2f10d808c811274074849e2d64882ce9cf
unknown <ajibolamubarak304@gmail.com>
2022-06-28 13:31:49 +0100

    working on the video HTML section 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ let swiper = new Swiper(".discover__container", {
   centeredSlides: true,
   slidesPerView: "auto",
   loop: true,
-  spaceBetween: 30,
+  spaceBetween: 10,
   coverflowEffect: {
     rotate: 0,
   },
