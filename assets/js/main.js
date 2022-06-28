e6f8f831b08b0f5cbeaed88dcefafef7f6175534
unknown <ajibolamubarak304@gmail.com>
2022-06-28 13:00:15 +0100

    making some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,14 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
+/*==================== SWIPER DISCOVER ====================*/
 let swiper = new Swiper(".discover__container", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
   loop: true,
-  spaceBetween: 32,
+  spaceBetween: 30,
   coverflowEffect: {
     rotate: 0,
   },
